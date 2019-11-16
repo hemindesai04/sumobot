8602386dcedf5afaa048eab39a6aff0f1ce767d4
Hemin Desai <hemin_desai04@yahoo.com>
2019-11-15 23:03:22 -0500

    Update using_i2cmux_and_rfd77402.ino
    Adding support to use multiple(5) RFD77402 sensors along with the i2c mux.

--- using_i2cmux_and_rfd77402.ino
+++ using_i2cmux_and_rfd77402.ino
@@ -10,7 +10,12 @@
 // i2c mux address
 #define TCAADDR 0x70
 
-RFD77402 myDistance; //Hook object to the library
+//Hook objects to the library
+RFD77402 myDistance1;
+RFD77402 myDistance2;
+RFD77402 myDistance3;
+RFD77402 myDistance4;
+RFD77402 myDistance5;
 
 /**
  *  Function to initialize sensors connected to i2c mux 
@@ -23,24 +28,45 @@ void tca_select (uint8_t i) {
   Wire.endTransmission();
 }
 
-int get_distance(int sensor) {
+/**
+ * Function to get distance of the nearest object
+ * using the given sensor.
+ */
+int get_distance(int sensor, RFD77402 myDistance) {
   tca_select(sensor);
   myDistance.takeMeasurement();
   return myDistance.getDistance(); //Retrieve the distance value
 }
 
+/**
+ * Helper function to print distance of the nearest object from
+ * the given sensor.
+ */
+void print_helper(int number, int distance) {
+  String disp_string = "Sensor " + number;
+  String dist_string = disp_string + ": distance = ";
+  Serial.print(dist_string);
+  Serial.print(distance);
+  Serial.print("mm");
+  Serial.println();
+}
+
 void setup() {
-  // initialize sensor1 connected to SD0 and SC0
-  tca_select(0);
+  // initialize sensor1 through sensor5 connected to 
+  // SD0 and SC0 through SD4 and SC4
+  for (int i = 0; i < 5; i++) {
+    tca_select(i);
+  }
   Serial.begin(9600);
   while (!Serial);
   Serial.println("RFD77402 Testing");
 }
 
 void loop() {
-  unsigned int distance = get_distance(0);
-  Serial.print("distance: ");
-  Serial.print(distance);
-  Serial.print("mm");
-  Serial.println();
+  RFD77402 distance[5] = {myDistance1, myDistance2, myDistance3,
+                          myDistance4,myDistance5};
+  for (int i = 0; i < 5; i++) {
+    unsigned int distance_mm = get_distance(i, distance[i]);
+    print_helper(i, distance_mm);
+  }
 }
